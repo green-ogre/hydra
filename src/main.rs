8692a60f54ccf3a4f79b4cbfdd3e3f261c68c2d9
green-ogre <109698029+green-ogre@users.noreply.github.com>
2024-09-10 14:33:28 -0600

    moved emulator into a new repo

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,39 @@
 use colored::Colorize;
-use core::panic;
 use std::{os::unix::process::CommandExt, path::PathBuf, process::Command, str::FromStr};
 
 mod ast;
 mod compiler;
-mod emulator;
 mod parse;
 mod token;
 
 fn main() {
     // riscv32-unknown-elf-gcc -nostdlib -Ttext=0x80000000 -o emu.txt ./emu.S
-    let output = Command::new("riscv32-unknown-elf-gcc")
-        .arg("-nostdlib")
-        .arg("-Ttext=0x80000000")
-        .arg("-oemu.txt")
-        .arg("./emu.S")
-        .output()
-        .unwrap();
-    if !output.stdout.is_empty() {
-        println!("{}", String::from_utf8_lossy(&output.stdout));
-    }
-    if !output.stderr.is_empty() {
-        println!("{}", String::from_utf8_lossy(&output.stderr));
-    }
-    // riscv32-unknown-elf-objcopy -O binary emu.bin
-    let output = Command::new("riscv32-unknown-elf-objcopy")
-        .arg("-O")
-        .arg("binary")
-        .arg("emu.txt")
-        .output()
-        .unwrap();
-    if !output.stdout.is_empty() {
-        println!("{}", String::from_utf8_lossy(&output.stdout));
-    }
-    if !output.stderr.is_empty() {
-        println!("{}", String::from_utf8_lossy(&output.stderr));
-    }
-
-    let raw = std::fs::read("./emu.txt").unwrap();
-
-    // std::fs::remove_file("./emu.txt").unwrap();
-
-    emulator::emulate(&raw);
+    // let output = Command::new("riscv32-unknown-elf-gcc")
+    //     .arg("-nostdlib")
+    //     .arg("-Ttext=0x80000000")
+    //     .arg("-oemu.txt")
+    //     .arg("./emu.S")
+    //     .output()
+    //     .unwrap();
+    // if !output.stdout.is_empty() {
+    //     println!("{}", String::from_utf8_lossy(&output.stdout));
+    // }
+    // if !output.stderr.is_empty() {
+    //     println!("{}", String::from_utf8_lossy(&output.stderr));
+    // }
+    // // riscv32-unknown-elf-objcopy -O binary emu.bin
+    // let output = Command::new("riscv32-unknown-elf-objcopy")
+    //     .arg("-O")
+    //     .arg("binary")
+    //     .arg("emu.txt")
+    //     .output()
+    //     .unwrap();
+    // if !output.stdout.is_empty() {
+    //     println!("{}", String::from_utf8_lossy(&output.stdout));
+    // }
+    // if !output.stderr.is_empty() {
+    //     println!("{}", String::from_utf8_lossy(&output.stderr));
+    // }
 
     // let mut args = std::env::args();
     // let _ex = args.next();
